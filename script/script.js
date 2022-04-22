e557dbce77a3d3c2fb6799b6b1158f07ffe6e4d0
Marcelo evangelista <57013425+veih@users.noreply.github.com>
2022-04-22 17:58:14 -0300

    foram feitas as configurações e instalações do jquery

---
+++ script/script.js
@@ -0,0 +1,10 @@
+function start() { //inicio da funcao start
+
+    $("inicio").hide();
+    
+    $("backgroundGame").append("<div id='play'></div>");
+    $("backgroundGame").append("<div id='enemy1'></div>");
+    $("backgroundGame").append("<div id='enemy2'></div>");
+    $("backgroundGame").append("<div id='friend'></div>");
+
+} //fim da funcao start
