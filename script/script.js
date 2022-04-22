7cde53b7df5be5797f332c5c1597ebf45786d461
Marcelo evangelista <57013425+veih@users.noreply.github.com>
2022-04-22 18:10:41 -0300

    alterações pendentes

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ function start() { //inicio da funcao start
 
     $("inicio").hide();
     
-    $("backgroundGame").append("<div id='play'></div>");
+    $("backgroundGame").append("<div id='jogador'></div>");
     $("backgroundGame").append("<div id='enemy1'></div>");
     $("backgroundGame").append("<div id='enemy2'></div>");
     $("backgroundGame").append("<div id='friend'></div>");
